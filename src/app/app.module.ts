bb7fef962db6915a89351216e4ba336cbaa808bf
Eduard <eduard.duarte@hotmail.com>
2019-03-31 17:42:48 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatCheckboxModule} from '@angular/material';
+import { PruebasComponent } from './pruebas/pruebas.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainNavComponent,
+    PruebasComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    RouterModule.forRoot([
+      {path: 'prueba', component: PruebasComponent}
+    ]),
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
