c2431a5ee677717590925830cd2147c79ec8f27c
Eduard <eduard.duarte@hotmail.com>
2019-04-04 23:40:06 -0500

    add card material in child

--- src/app/commBetweenComponents/child-tarjeta/child-tarjeta.component.ts
+++ src/app/commBetweenComponents/child-tarjeta/child-tarjeta.component.ts
@@ -3,7 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-child-tarjeta',
   templateUrl: './child-tarjeta.component.html',
-  styles: []
+  styles: [`
+    .example-card {
+    max-width: 300px;
+  }
+
+  .example-header-image {
+    background-image: url('https://material.angular.io/assets/img/examples/shiba1.jpg');
+    background-size: cover;
+  }`]
 })
 export class ChildTarjetaComponent implements OnInit {
 
@@ -11,5 +19,19 @@ export class ChildTarjetaComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  evento = {
+    id: 1,
+    name: 'Conference Mac',
+    date: '14/02/86',
+    time: '10:00 am',
+    price: 250000,
+    imageUrl: 'https://www.panorama.com.ve/__export/1493040430239/sites/panorama/img/cienciaytecnologia/2017/04/24/tecnologia.jpg_390504426.jpg',
+    location:{
+      address: 'kr 56 # 151 - 51',
+      city: 'Bogotá',
+      country: 'Colombia'
+    }
+  };
 
 }
--- src/app/commBetweenComponents/commBetweenC.module.ts
+++ src/app/commBetweenComponents/commBetweenC.module.ts
@@ -3,15 +3,19 @@ import { RouterModule } from '@angular/router';
 
 import { ChildTarjetaComponent } from './child-tarjeta/child-tarjeta.component'
 import { ParentOutputComponent } from './parent-output/parent-output.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations:[
     ChildTarjetaComponent,
     ParentOutputComponent,
+
   ],
   imports:[
+    MatCardModule,
     RouterModule.forChild([
-      {path: 'childTarjet', component: ChildTarjetaComponent}
+      {path: 'childTarjet', component: ChildTarjetaComponent},
+      {path: 'parentOuput', component: ParentOutputComponent}
     ]),
   ]
 })
